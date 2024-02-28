7366dbee21a3d40236a29c89ca07e65d99e2b21e
Dmitriy Rychek <dimarychek@gmail.com>
2024-02-28 12:06:38 +0200

    Initial Commit

---
+++ SwiftUI Concepts/SwiftUI_ConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ConceptsApp.swift
+//  SwiftUI Concepts
+//
+//  Created by Dmitriy Rychek on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
